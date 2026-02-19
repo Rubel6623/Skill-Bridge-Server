7e6dcd23928cd4ac971766de459ceaddd3115e71
Rubel Rudra <rubelrudra27@gmail.com>
2026-02-19 21:11:50 +0600

    Create a User model by prisma

---
+++ prisma/migrations/20260219145935_init/migration.sql
@@ -0,0 +1,28 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('STUDENT', 'TUTOR', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "BookingStatus" AS ENUM ('CONFIRMED', 'COMPLETED', 'CANCELLED');
+
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('ACTIVE', 'BANNED');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL,
+    "status" "Status" NOT NULL DEFAULT 'ACTIVE',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE INDEX "users_status_idx" ON "users"("status");
